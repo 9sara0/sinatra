0034583ab65af05714c2980739dc7fa1b6f85955
6eff <6eff@mail.ru>
2016-07-19 14:57:19 +0300

    uses params

--- app.rb
+++ app.rb
@@ -9,7 +9,13 @@ get '/secret' do
   "it's a BIG secret"
 end
 
-get '/cat' do
+get '/random-cat' do
   @random_name = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  p params
+  @random_name = params[:name]
   erb :index
 end
